f37ef4b4e1ca97d299cd9552f3ef985fe365ba8d
jcreatvz <jcreatvz@gmail.com>
2022-01-29 17:48:18 -0700

    added new files

---
+++ SL/story_content/6J2jQ6TUjq6_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.34.20804.0%0D%0A%0D%0A00:00:00.096%20--%3E%2000:00:02.656%0D%0AFirst%20we%20will%20do%20an%20overall%20inspection%20of%20the%20unit.%20%0D%0A%0D%0A"}')
---
+++ SL/story_content/user.js
@@ -0,0 +1,43 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6l2n7qF9VvL":
+        Script1();
+        break;
+      case "6kTisp564NT":
+        Script2();
+        break;
+      case "64udXrUHBU8":
+        Script3();
+        break;
+      case "5YGAnpBg7nx":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.body.style.background = 'url(http://bombardier.xpandev.ca/nyct/CouplerAdapter/bg.png) no-repeat';
+document.body.style.backgroundSize = 'cover';
+}
+
+function Script2()
+{
+  document.body.style.background = 'url(http://bombardier.xpandev.ca/nyct/CouplerAdapter/bg.png) no-repeat';
+document.body.style.backgroundSize = 'cover';
+}
+
+function Script3()
+{
+  document.body.style.background = 'url(http://bombardier.xpandev.ca/nyct/CouplerAdapter/bg.png) no-repeat';
+document.body.style.backgroundSize = 'cover';
+}
+
+function Script4()
+{
+  document.body.style.background = 'url(http://bombardier.xpandev.ca/nyct/CouplerAdapter/bg.png) no-repeat';
+document.body.style.backgroundSize = 'cover';
+}
+
